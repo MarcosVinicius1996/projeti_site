6a81c686d06d63e9e8629c3dc769e681c19bd816
MarcosVinicius1996 <115051272+MarcosVinicius1996@users.noreply.github.com>
2022-10-20 20:32:45 -0300

    Add files via upload

---
+++ Site/calcular.js
@@ -0,0 +1,18 @@
+var gasolina, etanol;
+
+function melhor(){
+    etanol= parseFloat(frdados.txetanol.value.replace(",","."));
+    gasolina= parseFloat(frdados.txgasolina.value.replace(",","."));
+    if(etanol < 0.7*gasolina){
+        document.getElementById("imagem").src="imagens/alcool.png";
+
+    }
+
+    else{
+        document.getElementById("imagem").src="imagens/gasolina.png";
+    }
+}
+
+function limpar(){
+    document.getElementById("imagem").src="imagens/normal.png";
+}
